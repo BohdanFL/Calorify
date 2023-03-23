a115afedf4568951a5fd4cc483ee56508ab0e2fa
BohdanFlexer <robot228rb@gmail.com>
2023-03-23 13:35:43 +0200

    init

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVVBYTd3VC8vLy8vQUFBQm1YOC8xdlVoakVYcm1BSHIvRGNnSmNOTS9USTd2c05MWVc4bEswdzFTQVE0ZkM5NEJ1b05sNDY1L3pQWkRyVEFaakc5ay9pTC9PMXh1OTE5TlRvNHQzd3pzdm42dTZDQ2UvQlpseDBDWkJqWmtKWHdNS3dNYisyQXB5NEdQdHZheTlGdnR1bExRdURvQkFlOWhWMGtZbE55MkxsdFZyWC9OUStFT2JjQmJjSEIzdVpsMGhJTjNUQ01PZlFuemFBSElDbS9MUEE4a2hNQTFzWjk1cmVBVi9yaUpTTEtKUmpUdmlHODNyK0FBQlpaQnJkcTdvdFFYNmV3SjNrMUJIVXhvd3F6RzlIY21UN2wzMzJZN09DZTdrWVZyeXlaa05ndXdQa3dqaE45b1RQZXhYOGFsbldTYjRIMWx2QnBya1hpTTlvYytOODdGcGQ3NEQ4blRBTnR3QlV3dmlTRXpXYjlHeWlGSnVGeHpSSGw=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
