3161f8483ad5023bb845880c68b02eb6854a4a49
Maxym <989m66@gmail.com>
2023-04-15 22:30:40 +0300

    Ability to swap panels

---
+++ Assets/Scripts/UI/PanelChanger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelChanger : MonoBehaviour
+{
+    public GameObject[] panels;
+
+    public void LoadPanelByIndex(int index)
+    {
+        for(int i = 0; i < panels.Length; i++)
+        {
+            if(index != i)
+            {
+                panels[i].SetActive(false);
+            } else
+            {
+                panels[i].SetActive(true);
+            }
+        }
+    }
+}
